323c541ecdfb2f06660465acf6d290e238ea459c
Igor Kaplya <igor.kaplya@mail.ru>
2020-07-17 08:11:25 +0300

    Доработал тест процессора
     * Переименовал методы
     * доб. проверку обработки страны

--- test/Ttn.Test.Processor.pas
+++ test/Ttn.Test.Processor.pas
@@ -20,28 +20,27 @@ type
     procedure Setup;
     [TearDown]
     procedure TearDown;
-    // Sample Methods
-    // Simple single Test
+    // Test with TestCase Attribute to supply parameters.
     [Test]
-    procedure Test1;
+    [TestCase('','RUB')]
+    [TestCase('','РублЬ')]
+    [TestCase('','Sehenswürdigkeiten')]
+    procedure TestProcessCurrency(const ACurrencyExpected: string);
     // Test with TestCase Attribute to supply parameters.
     [Test]
     [TestCase('','1.001,1001')]
     [TestCase('','0.000001,0.001')]
     [TestCase('','0,0')]
     [TestCase('','-3.1415,-3141.5')]
-    procedure ProcessWeight(const AWeight, AWeightExpected: Double);
-    // Test with TestCase Attribute to supply parameters.
+    procedure TestProcessWeight(const AWeight, AWeightExpected: Double);
     [Test]
-    [TestCase('','RUB')]
-    [TestCase('','РублЬ')]
-    [TestCase('','Sehenswürdigkeiten')]
-    procedure ProcessCurrency(const ACurrencyExpected: string);
+    procedure TestProcessCountry;
   end;
 
 implementation
 
 uses
+  dmMain,
   Ttn.Registration;
 
 procedure TTtnTestProcessor.Setup;
@@ -55,12 +54,7 @@ begin
   FProcessor := nil;
 end;
 
-procedure TTtnTestProcessor.Test1;
-begin
-  Assert.Pass();
-end;
-
-procedure TTtnTestProcessor.ProcessWeight(const AWeight, AWeightExpected: Double);
+procedure TTtnTestProcessor.TestProcessWeight(const AWeight, AWeightExpected: Double);
 begin
   ProcessObject.WEIGHT1 := AWeight;
   Processor.ProcessWeight(ProcessObject);
@@ -69,11 +63,66 @@ begin
   Assert.AreEqual(AWeightExpected, ProcessObject.WEIGHT3);
 end;
 
-procedure TTtnTestProcessor.ProcessCurrency(const ACurrencyExpected: string);
+procedure TTtnTestProcessor.TestProcessCurrency(const ACurrencyExpected: string);
 begin
   Processor.Currency := ACurrencyExpected;
   Processor.ProcessCurrency(ProcessObject);
   Assert.AreEqual(ACurrencyExpected, ProcessObject.VAL);
+end;
+
+procedure TTtnTestProcessor.TestProcessCountry;
+
+  function callProcessCountry(const AInput: String): TTestLocalMethod;
+  begin
+    Result :=
+    (
+      procedure()
+      begin
+        ProcessObject.ErrorMsg := '';
+        ProcessObject.STR_PR := AInput;
+        Processor.ProcessCountry(ProcessObject);
+      end
+    );
+  end;
+
+type
+  RCountryTest = record
+    input,
+    expected: string;
+  end;
+const
+  accepted_as_GB: array[0..1] of string = ('великобритания','unighted kingdom');
+  country_Val_GB = 'GB';
+  valid_Input: array[0..3] of RCountryTest = (
+    (input: 'Великобритания'; expected: country_Val_GB),
+    (input: 'Страна происхождения - Великобритания'; expected: country_Val_GB),
+    (input: 'Страна происхождения - "Великобритания"'; expected: country_Val_GB),
+    (input: 'Unighted Kingdom"'; expected: country_Val_GB)
+  );
+  wrong_Input: array[0..1] of string = (
+    '',
+    'Виликабритания АЗАЗАЗА'
+  );
+var
+  testValid: RCountryTest;
+  countryKeyGB: string;
+  wrongCountry: string;
+begin
+  for countryKeyGB in accepted_as_GB do
+    Processor.CountryList.AddOrSetValue(countryKeyGB, country_Val_GB);
+  for testValid in valid_Input do
+  begin
+    Assert.WillNotRaise(callProcessCountry(testValid.input));
+    Assert.AreEqual(testValid.expected, ProcessObject.str_Pr);
+    Assert.IsEmpty(ProcessObject.ErrorMsg);
+  end;
+  for wrongCountry in wrong_Input do
+  begin
+    Assert.WillNotRaise(callProcessCountry(wrongCountry));
+    Assert.AreEqual(wrongCountry, ProcessObject.str_Pr);
+    Assert.IsNotEmpty(ProcessObject.ErrorMsg);
+  end;
+
 end;
 
 initialization
