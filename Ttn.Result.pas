277e855da86d3d364ea2cee69572a16d8e51f16f
Igor Kaplya <igor.kaplya@mail.ru>
2020-10-10 11:07:32 +0300

    Added Ttn Result implementation

---
+++ Ttn.Result.pas
@@ -0,0 +1,101 @@
+unit Ttn.Result;
+
+interface
+
+uses
+  Ttn.Interfaces;
+
+type
+
+  TTtnResult = class(TInterfacedObject, ITtnResult)
+  private
+    FDateTtn: TDate;
+    FDestinationCountry: string;
+    FDestinationCountryRegion: StringCountryRegion;
+    FFileName: string;
+    FShipmentCountry: string;
+    FShipmentCountryRegion: StringCountryRegion;
+    function GetDateTtn: TDate;
+    function GetDestinationCountry: string;
+    function GetDestinationCountryRegion: StringCountryRegion;
+    function GetFileName: string;
+    function GetShipmentCountry: string;
+    function GetShipmentCountryRegion: StringCountryRegion;
+    procedure SetDateTtn(const Value: TDate);
+    procedure SetDestinationCountry(const Value: string);
+    procedure SetDestinationCountryRegion(const Value: StringCountryRegion);
+    procedure SetFileName(const Value: string);
+    procedure SetShipmentCountry(const Value: string);
+    procedure SetShipmentCountryRegion(const Value: StringCountryRegion);
+  public
+    property FileName: string read GetFileName write SetFileName;
+    property DestinationCountry: string read GetDestinationCountry write SetDestinationCountry;
+    property DestinationCountryRegion: StringCountryRegion read GetDestinationCountryRegion write SetDestinationCountryRegion;
+    property ShipmentCountry: string read GetShipmentCountry write SetShipmentCountry;
+    property ShipmentCountryRegion: StringCountryRegion read GetShipmentCountryRegion write SetShipmentCountryRegion;
+    property DateTtn: TDate read GetDateTtn write SetDateTtn;
+  end;
+
+implementation
+
+function TTtnResult.GetDateTtn: TDate;
+begin
+  Result := FDateTtn;
+end;
+
+function TTtnResult.GetDestinationCountry: string;
+begin
+  Result := FDestinationCountry;
+end;
+
+function TTtnResult.GetDestinationCountryRegion: StringCountryRegion;
+begin
+  Result := FDestinationCountryRegion;
+end;
+
+function TTtnResult.GetFileName: string;
+begin
+  Result := FFileName;
+end;
+
+function TTtnResult.GetShipmentCountry: string;
+begin
+  Result := FShipmentCountry;
+end;
+
+function TTtnResult.GetShipmentCountryRegion: StringCountryRegion;
+begin
+  Result := FShipmentCountryRegion;
+end;
+
+procedure TTtnResult.SetDateTtn(const Value: TDate);
+begin
+  FDateTtn := Value;
+end;
+
+procedure TTtnResult.SetDestinationCountry(const Value: string);
+begin
+  FDestinationCountry := Value;
+end;
+
+procedure TTtnResult.SetDestinationCountryRegion(const Value: StringCountryRegion);
+begin
+  FDestinationCountryRegion := Value;
+end;
+
+procedure TTtnResult.SetFileName(const Value: string);
+begin
+  FFileName := Value;
+end;
+
+procedure TTtnResult.SetShipmentCountry(const Value: string);
+begin
+  FShipmentCountry := Value;
+end;
+
+procedure TTtnResult.SetShipmentCountryRegion(const Value: StringCountryRegion);
+begin
+  FShipmentCountryRegion := Value;
+end;
+
+end.
