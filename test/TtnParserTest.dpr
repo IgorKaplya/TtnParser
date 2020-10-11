f45f966bd27b9a4362ec286bff288150cf9c4c9c
Igor Kaplya <igor.kaplya@mail.ru>
2020-10-11 12:06:29 +0300

    Updated project structure

--- test/TtnParserTest.dpr
+++ test/TtnParserTest.dpr
@@ -1,27 +1,15 @@
 program TtnParserTest;
 
-{$IFNDEF TESTINSIGHT}
-  {$APPTYPE CONSOLE}
-{$ENDIF}
-
 uses
   Ttn.Test.Run.Console in 'Ttn.Test.Run.Console.pas',
   Ttn.Test.Run.Testinsight in 'Ttn.Test.Run.Testinsight.pas',
-  Ttn.Registration in '..\Ttn.Registration.pas',
   Ttn.Test.Parser in 'Ttn.Test.Parser.pas',
   Ttn.Test.Processor in 'Ttn.Test.Processor.pas',
   Ttn.Test.ResultStorage in 'Ttn.Test.ResultStorage.pas',
   Ttn.Test.TtnList in 'Ttn.Test.TtnList.pas',
-  Ttn.Test.TtnObj in 'Ttn.Test.TtnObj.pas';
+  Ttn.Test.TtnObj in 'Ttn.Test.TtnObj.pas',
+  Ttn.Test.Run in 'Ttn.Test.Run.pas';
 
 begin
-  try
-    RegisterTypes();
-    {$IFDEF TESTINSIGHT}
-      TestTestinsightRun();
-    {$ELSE}
-      TestConsoleRun();
-    {$ENDIF}
-  except
-  end;
+  RunTests();
 end.
