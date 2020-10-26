286e940521af3f65b632ca3098a2a39d303f17d0
Igor Kaplya <igor.kaplya@mail.ru>
2020-10-26 18:16:46 +0300

    Added fileversion to app caption

--- frmTtnParser.pas
+++ frmTtnParser.pas
@@ -435,6 +435,37 @@ if FInpFile<>Value then
 end;
 
 procedure TfrmTtnParserMain.StartUp;
+
+  function GetBuildInfo(): string;
+  {From Steve Schafer }
+  var
+    V1, V2, V3, V4: Word;
+    VerInfoSize: DWORD;
+    VerInfo: pointer;
+    VerValueSize: DWORD;
+    VerValue: PVSFixedFileInfo;
+    Dummy: DWORD;
+  begin
+    VerInfoSize := GetFileVersionInfoSize(PChar(ParamStr(0)), Dummy);
+    GetMem(VerInfo, VerInfoSize);
+    GetFileVersionInfo(PChar(ParamStr(0)), 0, VerInfoSize, VerInfo);
+    VerQueryValue(VerInfo, '\', pointer(VerValue), VerValueSize);
+    with VerValue^ do
+      begin
+        V1 := dwFileVersionMS shr 16;
+        V2 := dwFileVersionMS and $FFFF;
+        V3 := dwFileVersionLS shr 16;
+        V4 := dwFileVersionLS and $FFFF;
+      end;
+    FreeMem(VerInfo, VerInfoSize);
+    Result := Format('%d.%d.%d.%d',[V1, V2, V3, V4])
+  end;
+
+  procedure LoadActualFileVersion();
+  begin
+    Self.Caption := Self.Caption + ' v' + GetBuildInfo();
+  end;
+
   procedure IniRead();
   {Чтение ini в память}
   var
@@ -472,6 +503,7 @@ procedure TfrmTtnParserMain.StartUp;
 begin
   if not Inited then
   try
+    LoadActualFileVersion();
     IniRead();
     DbConnect();
     InitTab();
@@ -518,7 +550,6 @@ begin
     ResultStorage.ActiveResult := ResultStorage[Sender.GetFirstSelected().Index];
     if ResultStorage.ActiveResult.TtnList.Count = 0 then
       ResultStorage.ActiveResult.Load();
-    lblActiveResultInfo.Caption := Format('', []);
     cpResultStorage.ActiveCard := crdActiveResult;
     edtShipmentCountry.Text := ResultStorage.ActiveResult.ShipmentCountry;
     edtDeliveryCountry.Text := ResultStorage.ActiveResult.DestinationCountry;
